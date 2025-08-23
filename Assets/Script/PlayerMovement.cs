6349471f070438eb8322cb896c30109318c52c7b
mijimoto <87632339+mijimoto@users.noreply.github.com>
2025-08-23 10:00:13 +0700

    demo part 3

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,19 +2,43 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject bulletPrefabs;
+public float shootingInterval;
+public Vector3 bulletOffset;
+
+private float lastBulletTime;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
- // Update is called once per frame
-void Update()
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(Input.mousePosition);
+        var worldPoint =
+        Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        worldPoint.z = 0;
+        transform.position = worldPoint;
+        
+        // firing postion 
+    if (Input.GetMouseButton(0))
+        {
+            UpdateFiring();
+        }
+    }
+private void UpdateFiring()
 {
-    Debug.Log(Input.mousePosition);
-var worldPoint =
-Camera.main.ScreenToWorldPoint(Input.mousePosition);
-worldPoint.z = 0;
-transform.position = worldPoint;
+if (Time.time - lastBulletTime > shootingInterval)
+{
+ShootBullet();
+lastBulletTime = Time.time;
+}
+}
+
+private void ShootBullet()
+{
+var bullet = Instantiate(bulletPrefabs, transform.position + bulletOffset, transform.rotation);
 }
 }
